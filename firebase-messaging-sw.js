675d7b33fb5b53a5d5869036143c793718b0c183
huy <dr.huy69@gmail.com>
2022-01-28 21:05:02 +0700

    update dev

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.24.0/firebase-messaging.js");
+firebase.initializeApp({
+  apiKey: "AIzaSyBQIxMWIyyh6ytQakhVWx3ueBqkmrbzK94",
+  authDomain: "aivis-camera.firebaseapp.com",
+  projectId: "aivis-camera",
+  storageBucket: "aivis-camera.appspot.com",
+  messagingSenderId: "201191751459",
+  appId: "1:201191751459:web:5577718e3b6b6cffa69696",
+  measurementId: "G-RJN1TN1PV1"
+});
+const messaging = firebase.messaging();
